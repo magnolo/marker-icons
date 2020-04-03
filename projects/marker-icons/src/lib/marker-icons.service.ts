87752a988ea81d924310bb6ac95ce67d429480c8
Magnolo <walder.manfred@gmail.com>
2020-04-03 17:15:18 +0200

    adding naming updates

--- projects/marker-icons/src/lib/marker-icons.service.ts
+++ projects/marker-icons/src/lib/marker-icons.service.ts
@@ -3,26 +3,43 @@ import { Injectable } from '@angular/core';
 import * as Markers from './../icons/marker-icons';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MarkerIconsService {
-
-  constructor() { }
+  constructor() {}
 
   public getIcon(iconName: string) {
     // console.log();
-    if (!Markers[iconName]) {
+    const name = 'marker' + this.capitalize(this.toCamelCase(iconName));
+  
+    if (!Markers[name]) {
       console.warn(
-        `We could not find the marker Icon with the name ${iconName}, did you add it to the Icon registry?`
+        `We could not find the marker Icon with the name ${iconName} by ${name}, did you add it to the Icon registry?`
       );
       return {
         name: '',
-        data: null
+        data: null,
       };
     }
     // return this.registry.get(iconName);
     // const icon = await import('../../icons');
 
-    return Markers[iconName].data;
+    return Markers[name].data;
+  }
+
+  private toCamelCase(name) {
+    return name
+      .split('-')
+      .reduce((a, b) => a + b.charAt(0).toUpperCase() + b.slice(1))
+      .split('_')
+      .reduce((a, b) => a + b.charAt(0).toUpperCase() + b.slice(1));
+  }
+
+  private capitalize(name){
+    if (typeof name !== 'string'){
+      return '';
+    }
+
+    return name.charAt(0).toUpperCase() + name.slice(1);
   }
 }
