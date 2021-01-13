1451aeae1b4c4bd9f8320276dd3e27c5bcda7069
Magnolo <walder.manfred@gmail.com>
2021-01-13 15:00:20 +0100

    adding showcase to the lib

---
+++ projects/showcase/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'marker-test';
+  size: number = 50;
+  color: string = '#ff0000';
+}
